boomer319/verrueckterschwarm2

commit 5c0249051066c10dc2885062ad9af22b636d70e5

crazyfie_py: correct handling of cfname vs cfid (#124)
-   deprecate the use of crazyflieById,
-  add a new dict "crazyflieByName", and
-  consistently use the by-name lookup internally.

Fixes #119.

crazyflie_py/crazyflie_py/crazyflie.py
@@ -89,20 +89,18 @@ class Crazyflie:
     The bulk of the module's functionality is contained in this class.
     """
 
-    def __init__(self, node, id, initialPosition):
+    def __init__(self, node, cfname, initialPosition):
         """Constructor.
 
         Args:
-            id (int): Integer ID in range [0, 255]. The last byte of the robot's
-                radio address.
+            cfname (string): Name of the robot names[ace]
             initialPosition (iterable of float): Initial position of the robot:
                 [x, y, z]. Typically on the floor of the experiment space with
                 z == 0.0.
             tf (tf.TransformListener): ROS TransformListener used to query the
                 robot's current state.
         """
-        self.id = id
-        prefix = "/cf" + str(id)
+        prefix = cfname
         self.prefix = prefix
         self.initialPosition = np.array(initialPosition)
         self.node = node
@@ -686,18 +684,17 @@ class CrazyflieServer(rclpy.node.Node):
 
         self.crazyflies = []
         self.crazyfliesById = dict()
+        self.crazyfliesByName = dict()
         for cfname, cfsettings in cfg["robots"].items():
             if cfsettings["enabled"]:
                 initialPosition = np.array(cfsettings["initial_position"])
-                if "id" in cfsettings:
-                    cfid = int(cfsettings["id"])
-                elif "uri" in cfsettings:
-                    cfid = int(cfsettings["uri"][-2:], 16)
-                print(cfid)
-                # cf = Crazyflie(id, initialPosition, self.tf)
-                cf = Crazyflie(self, cfid, initialPosition)
+                cf = Crazyflie(self, cfname, initialPosition)
                 self.crazyflies.append(cf)
-                self.crazyfliesById[cfid] = cf
+                self.crazyfliesByName[cfname] = cf
+                # For legacy crazyswarm1 code, also provide crazyfliesById
+                if "uri" in cfsettings:
+                    cfid = int(cfsettings["uri"][-2:], 16)
+                    self.crazyfliesById[cfid] = cf
 
     def emergency(self):
         """Emergency stop. Cuts power; causes future commands to be ignored.

crazyflie_py/crazyflie_py/crazyflieSim.py
@@ -174,10 +174,10 @@ class Crazyflie:
     MODE_LOW_VELOCITY = 4
 
 
-    def __init__(self, id, initialPosition, timeHelper):
+    def __init__(self, cfname, initialPosition, timeHelper):
 
         # Core.
-        self.id = id
+        self.cfname = cfname
         self.groupMask = 0
         self.initialPosition = np.array(initialPosition)
         self.time = lambda: timeHelper.time()
@@ -459,16 +459,17 @@ class CrazyflieServer:
 
         self.crazyflies = []
         self.crazyfliesById = dict()
+        self.crazyfliesByName = dict()
         for cfname, cfsettings in cfg["robots"].items():
             if cfsettings["enabled"]:
                 initialPosition = cfsettings["initial_position"]
-                if "id" in cfsettings:
-                    cfid = int(cfsettings["id"])
-                elif "uri" in cfsettings:
-                    cfid = int(cfsettings["uri"][-2:], 16)
-                cf = Crazyflie(cfid, initialPosition, timeHelper)
+                cf = Crazyflie(cfname, initialPosition, timeHelper)
+                self.crazyfliesByName[cfname] = cf
                 self.crazyflies.append(cf)
-                self.crazyfliesById[cfid] = cf
+                # For legacy crazyswarm1 code, also provide crazyfliesById
+                if "uri" in cfsettings:
+                    cfid = int(cfsettings["uri"][-2:], 16)
+                    self.crazyfliesById[cfid] = cf
 
         self.timeHelper = timeHelper
         self.timeHelper.crazyflies = self.crazyflies

commit 37d827431c2ad87d47d670b1b1b0ed78ddc683bf

crazyflie_py: fix setParam regression (#128)
Introduced in 5c0249051066c10dc2885062ad9af22b636d70e5

Fixes #127.

crazyflie_py/crazyflie_py/crazyflie.py
@@ -100,7 +100,7 @@ class Crazyflie:
             tf (tf.TransformListener): ROS TransformListener used to query the
                 robot's current state.
         """
-        prefix = cfname
+        prefix = "/" + cfname
         self.prefix = prefix
         self.initialPosition = np.array(initialPosition)
         self.node = node